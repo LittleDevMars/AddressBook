386d93c73b5216c2036ad0c0756ac447552f6580
Kick2Kick <motonhj82@gmail.com>
2022-01-16 05:25:29 +0900

    주소록 수정중

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ enum Menu
     AddressSearch(u8),
 }
 
-pub struct HumanInfomation
+pub struct HumanAddressData
 {
     UserName : String,
     Address : String,
 }
 
-impl HumanInfomation
+impl HumanAddressData
 {
-    pub fn ShowInfomation(self)
+    pub fn ShowInfomation(&self)
     {
         println!("Infomation : {} , {}",self.UserName,self.Address);
     }
@@ -25,30 +25,42 @@ pub fn showMenu()
     println!("2. 주소 검색");
 }
 
-pub struct HumanInfomationSystem
+pub struct HumanAddressInfomation
 {
-    InfomationDatas : Vec<HumanInfomation>,
+    InfomationDatas : Vec<HumanAddressData>,
 }
 
-impl HumanInfomationSystem
+impl HumanAddressInfomation
 {
     pub fn add(&mut self, name : String, address : String)
     {
-        let data = HumanInfomation{ UserName: name, Address: address };
+        let data = HumanAddressData{ UserName: name, Address: address };
 
         self.InfomationDatas.push(data);
-    }
-}
-fn main() {
-
-    
-    let mut lineBuf = String::new();
-    loop 
-    {
-        showMenu();
-
-        io::stdin().read_line(&mut lineBuf);
+        
         
     }
-    
+
+    pub fn sort(&mut self)
+    {
+        if self.InfomationDatas.len() % 3  == 0
+        {
+            //self.InfomationDatas.sort_by_key(|sorce| sorce.UserName.get(0..1) );
+        }
+    }
+
+    pub fn remove(&mut self, name : &str) -> bool
+    {
+        true
+    }
+
+}
+
+
+fn main() {
+
+    let mut addressInfomationSystem = HumanAddressInfomation{ InfomationDatas: Vec::new() };
+
+    addressInfomationSystem.add(String::from("현준"),String::from("서울 특별시"));
+
 }
